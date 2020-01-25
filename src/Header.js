9a1d3226fdf9e25a1dedacdd8da4a9375610ce7f
VladislavPanichkin <54220242+VladislavPanichkin@users.noreply.github.com>
2020-01-25 18:37:28 +0300

    9 уроков

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (<div>
+        <a>Home</a>
+        <a>News Feed</a>
+        <a>Other</a>
+    </div>)
+}
+
+export default Header;
