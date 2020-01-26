1223c48b16a1534149ca8dc222b1f53a30feafe1
VladislavPanichkin <54220242+VladislavPanichkin@users.noreply.github.com>
2020-01-26 17:28:12 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Header from './Header.js'
-import Technologies from './Technologies.js';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://vk.com/images/brand/vk-logo--fb.png'></img>
+      </header>
+      <nav className='nav'>
+        <div>
+          <a>
+            Profile
+          </a>
+        </div>
+        <div>
+          <a>
+            Messages
+          </a>
+        </div>
+        <div>
+          <a>
+            News
+          </a>
+        </div>
+        <div>
+          <a>
+            Music
+          </a>
+        </div>
+        <div>
+          <a>
+            Settings
+          </a>
+        </div>
+      </nav>
+      <div className='content'>
+        <div>
+          <img src='https://openimagedenoise.github.io/images/moana_16spp_oidn.jpg'/>
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My Posts
+          <div>
+            New Post
+          </div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
