9343c2d3a21291124da11384e34563d72c97307e
Пил Пол <a@MacBook-Pro-Pil.local>
2020-04-01 19:31:19 +0300

    28

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -6,21 +6,8 @@ import Message from './Message/Message'
 
 const Dialogs = (props) => {
 
-    let dialogs = [
-        { id: 1, name: "Vlad" },
-        { id: 2, name: "Dalv" },
-        { id: 3, name: "Lavd" },
-        { id: 4, name: "Alvd" },
-    ]
-
-    let messages = [
-        { id: 1, message: "Hi" },
-        { id: 2, message: "How're you?" },
-        { id: 3, message: "Hey" },
-    ]
-
-    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
-    let messagesElements = messages.map(m => <Message message={m.message} id={m.id} />);
+    let dialogsElements = props.dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
+    let messagesElements = props.messages.map(m => <Message message={m.message} id={m.id} />);
 
     return (
         <div className={s.dialogs}>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import s from './MyPosts.module.css';
-import Post from './Post/Post'
+import Post from './Post/Post';
+import { NavLink } from 'react-router-dom';
 
-const MyPosts = () => {
+const MyPosts = (props) => {
 
-    let posts = [
+    /* let posts = [
         {id: 1, message: "Hi, how are you?", likes: "15"},
         {id: 2, message: "It's my first post", likes: "20"}
-    ]
+    ] */
 
-    let postsElements = posts.map( p => <Post message={p.message} id={p.id} likes={p.likes} />);
+    let postsElements = props.posts.map( p => <Post message={p.message} id={p.id} likes={p.likes} />);
 
     return <div className={s.postsBlock}>
         <h3>My Posts</h3>
@@ -20,7 +21,7 @@ const MyPosts = () => {
         <button>Add Post</button>
         </div>
         <div className={s.posts}>
-            { postsElements }
+             {postsElements}
         </div>
     </div>
 }
