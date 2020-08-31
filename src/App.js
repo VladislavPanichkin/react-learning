676e20506b17cd97de87ffe0e3ee4f894a84640c
Пил Пол <a@MacBook-Pro-Pil.local>
2020-08-31 23:00:06 +0300

    59

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
-import Profile from './components/Profile/Profile';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import News from './components/News/News';
 import Music from './components/Music/Music';
@@ -10,6 +9,7 @@ import Settings from './components/Settings/Settings';
 import { Route, BrowserRouter } from "react-router-dom";
 import {store} from './redux/redux-store'; //BAD
 import UsersContainer from './components/Users/UsersContainer';
+import ProfileContainer from './components/Profile/ProfileContainer';
 
 const App = (props) => {
 
@@ -19,7 +19,7 @@ const App = (props) => {
       <Navbar friends={store.getState().sidebar.friends} />
       <div className='app-wrapper-content'>
         <Route path='/dialogs' render={() => <DialogsContainer />} />
-        <Route path='/profile' render={() => <Profile />} />
+        <Route path='/profile' render={() => <ProfileContainer />} />
         <Route path='/news' component={News} />
         <Route path='/music' component={Music} />
         <Route path='/settings' component={Settings} />
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,11 +1,14 @@
 const ADD_POST = 'ADD-POST'
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
+const SET_USER_PROFILE = 'SET_USER_PROFILE'
 
 let initialState = {
     posts: [
         { id: 1, message: "Hi, how are you?", likes: 15 },
         { id: 2, message: "It's my first post", likes: 20 }
     ],
+
+    profile: null,
 
     newPostText: "asaas"
 }
@@ -29,6 +32,10 @@ const profileReducer = (state = initialState, action) => {
                 newPostText: action.newText
             } 
         }
+
+        case SET_USER_PROFILE: {
+            return {...state, profile: action.profile}
+        }
         default:
             return state
     }
@@ -36,5 +43,6 @@ const profileReducer = (state = initialState, action) => {
 
 export const addPostActionCreator = () => ({ type: ADD_POST })
 export const updateNewPostTextActionCreator = (text) => ({ type: UPDATE_NEW_POST_TEXT, newText: text })
+export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile })
 
 export default profileReducer
