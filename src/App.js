067d0a4f10029992147e2446808b29ebbb93c544
Пил Пол <a@MacBook-Pro-Pil.local>
2020-09-01 18:36:48 +0300

    60

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = (props) => {
       <Navbar friends={store.getState().sidebar.friends} />
       <div className='app-wrapper-content'>
         <Route path='/dialogs' render={() => <DialogsContainer />} />
-        <Route path='/profile' render={() => <ProfileContainer />} />
+        <Route path='/profile/:userId?' render={() => <ProfileContainer />} />
         <Route path='/news' component={News} />
         <Route path='/music' component={Music} />
         <Route path='/settings' component={Settings} />
